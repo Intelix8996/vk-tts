3a08c4e2166b8ecd340df615e5c2393f75da64b8
Intelix8996 <nikolat-repin201303@mail.ru>
2018-07-24 14:47:57 +0700

    Added many features

--- vk-tts/MainWindow.xaml.cs
+++ vk-tts/MainWindow.xaml.cs
@@ -20,6 +20,13 @@ using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using VkNet.Model.Attachments;
+using VkNet.Enums.SafetyEnums;
+using Newtonsoft.Json;
 
 namespace vk_tts
 {
@@ -32,10 +39,18 @@ namespace vk_tts
 
         string Login, Password;
 
+        VkNet.Utils.VkCollection<User> Friends;
+
+        long ChatID;
+
+        List<MediaAttachment> MessageAttachment = new List<MediaAttachment>();
+
+        LongPollServerResponse LongPollServer;
+
         public MainWindow()
         {
             InitializeComponent();
-
+            
             elementShow = new DoubleAnimation(1, TimeSpan.FromMilliseconds(250));
             elementHide = new DoubleAnimation(0, TimeSpan.FromMilliseconds(250));
             backgroundBlur = new DoubleAnimation(10, TimeSpan.FromMilliseconds(250));
@@ -61,7 +76,7 @@ namespace vk_tts
         private void loginButton_Click(object sender, RoutedEventArgs e)
         {
             Login = loginInputBox.Text;
-            Password = passwordInputBox.Text;
+            Password = passwordInputBox.Password;
 
             HideLoginField();
 
@@ -105,6 +120,147 @@ namespace vk_tts
 
                 File.WriteAllBytes(fileName, Encoder.Encode(ASCIIEncoding.ASCII.GetBytes(Login + " " + Password), ASCIIEncoding.ASCII.GetBytes(Login + " " + Password).Length));
             }
+
+            ConnectToLongPollServer();
+            RefreshChatList();
+        }
+
+        async void ConnectToLongPollServer()
+        {
+            LongPollServer = await api.Messages.GetLongPollServerAsync(true);
+
+            LongPollServerLoop(LongPollServer.Ts);
+        }
+
+        async void LongPollServerLoop(ulong ts)
+        {
+            string baseUrl = "https://" + LongPollServer.Server + "?act=a_check&key=" + LongPollServer.Key + "&ts=" + ts + "&wait=25&mode=2&version=2 ";
+            LongPollResponse Response;
+
+            using (WebClient clinet = new WebClient())
+            {
+                string rawResponse = await clinet.DownloadStringTaskAsync(new Uri(baseUrl));
+
+                if (rawResponse.Contains("title"))
+                {
+                    int startIdx = 0, endIdx = 0;
+
+                    for (int i = 2; i < rawResponse.Length - 1; ++i)
+                    {
+                        if (rawResponse[i] == '{')
+                            startIdx = i;
+
+                        if (rawResponse[i] == '}')
+                            endIdx = i;
+                    }
+
+                    rawResponse = rawResponse.Remove(startIdx - 1, endIdx - startIdx - 1);
+                }
+
+                Response = JsonConvert.DeserializeObject<LongPollResponse>(rawResponse);
+            }
+
+            HandleLongPollResponse(Response);
+            LongPollServerLoop(Response.ts);
+        }
+
+        void HandleLongPollResponse(LongPollResponse Response)
+        {
+
+            foreach (string[] rawCommand in Response.updates)
+            {
+                switch (rawCommand[0])
+                {
+                    case "4":
+
+                        if (ChatID != Convert.ToInt32(rawCommand[3]))
+                            Chat.AppendText("Пользователь " + GetNameByID(Convert.ToInt64(rawCommand[3])) + " отправил сообщение: " + rawCommand[5]);
+                        else if (GetNameByID(Convert.ToInt64(rawCommand[3])) != "Вы")
+                            Chat.AppendText(rawCommand[5] + " ---- " + GetNameByID(Convert.ToInt64(rawCommand[3])));
+                        break;
+                }
+            }
+        }
+
+        async void RefreshChatList()
+        {
+            ShowLoadingScreen();
+
+            CommandBinding Binding = new CommandBinding(ApplicationCommands.CancelPrint);
+
+            Binding.Executed += FriendChatChoose;
+
+            CommandBindings.Add(Binding);
+
+            Friends = await api.Friends.GetAsync(new FriendsGetParams {
+                Fields = ProfileFields.FirstName | ProfileFields.LastName | ProfileFields.Online | ProfileFields.Photo100,
+            });
+
+            foreach (var Friend in Friends)
+            {
+                ChatList.Items.Add(new System.Windows.Controls.Button {
+                    Content = Friend.FirstName + " " + Friend.LastName,
+                    Command = ApplicationCommands.CancelPrint,
+                    Opacity = 0,
+                    Width = 320,
+                });
+            }
+
+            foreach (System.Windows.Controls.Button Button in ChatList.Items)
+            {
+                Button.BeginAnimation(OpacityProperty, elementShow);
+            }
+
+            HideLoadingScreen();
+        }
+
+        private void FriendChatChoose(object sender, ExecutedRoutedEventArgs e)
+        {
+            System.Windows.Controls.Button CurrentButton = (System.Windows.Controls.Button)e.Source;
+
+            foreach (var Friend in Friends)
+            {
+                if (Friend.FirstName + " " + Friend.LastName == CurrentButton.Content.ToString())
+                    ChatID = Friend.Id;
+            }
+
+            RefreshChat();
+        }
+
+        async void RefreshChat()
+        {
+            ShowLoadingScreen();
+
+            Chat.Text = "";
+
+            var Messages = await api.Messages.GetHistoryAsync(new MessagesGetHistoryParams {
+                UserId = ChatID,
+                Count = 199,
+                Reversed = false,
+            });
+
+            foreach (var message in Messages.Messages.Reverse())
+            {
+                if (message.Text != "")
+                    Chat.AppendText("\n\n" + message.Text + " ---- " + GetNameByID((long)message.FromId));
+                else
+                    Chat.AppendText("\n\n Вложение ---- " + GetNameByID((long)message.FromId));
+            }
+
+            Chat.ScrollToEnd();
+
+            HideLoadingScreen();
+        }
+
+        string GetNameByID(long id)
+        {
+            foreach (var Friend in Friends)
+            {
+                if (Friend.Id == id)
+                    return Friend.FirstName + " " + Friend.LastName;
+            }
+
+            return "Вы";
         }
 
         string CheckPasswordChache()
@@ -120,6 +276,13 @@ namespace vk_tts
             }
 
             return null;
+        }
+
+        void RestartApp()
+        {
+            File.Delete(CheckPasswordChache());
+            Process.Start("vk-tts.exe");
+            Environment.Exit(0);
         }
 
         void ShowLoadingScreen()
@@ -142,6 +305,8 @@ namespace vk_tts
             loginInputBox.BeginAnimation(OpacityProperty, elementShow);
             passwordInputBox.BeginAnimation(OpacityProperty, elementShow);
             savePasswordCheckBox.BeginAnimation(OpacityProperty, elementShow);
+            loginIcon.BeginAnimation(OpacityProperty, elementShow);
+            passwordIcon.BeginAnimation(OpacityProperty, elementShow);
 
             BlurBackground();
         }
@@ -152,6 +317,8 @@ namespace vk_tts
             loginInputBox.BeginAnimation(OpacityProperty, elementHide);
             passwordInputBox.BeginAnimation(OpacityProperty, elementHide);
             savePasswordCheckBox.BeginAnimation(OpacityProperty, elementHide);
+            loginIcon.BeginAnimation(OpacityProperty, elementHide);
+            passwordIcon.BeginAnimation(OpacityProperty, elementHide);
 
             UnBlurBackground();
         }
@@ -163,6 +330,91 @@ namespace vk_tts
             capchaImage.BeginAnimation(OpacityProperty, elementShow);
 
             BlurBackground();
+        }
+
+        private void RestartButton_Click(object sender, RoutedEventArgs e)
+        {
+            RestartApp();
+        }
+
+        private void MessageEnterBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string Message = MessageEnterBox.Text;
+                MessageEnterBox.Text = "";
+
+                SendMessage(Message);
+            }
+        }
+
+        async void SendMessage(string message)
+        {
+            ShowLoadingScreen();
+
+            MessageAttachment.Clear();
+
+            if (speechButton.IsChecked == true)
+            {
+                string path = FormatPath(Environment.CurrentDirectory + "\\AudioCache\\" + message + ".wav");
+
+                SpeechSynthesizer Synth = new SpeechSynthesizer();
+                Synth.SetOutputToWaveFile(path);
+                Synth.Rate = 1;
+                Prompt prompt = Synth.SpeakAsync(message);
+
+                while (!prompt.IsCompleted)
+                {
+
+                }
+
+                Synth.Dispose();
+
+                byte[] file = File.ReadAllBytes(path);
+                MessageAttachment.Add(await LoadDocumentToChatAsync(api, file, DocMessageType.AudioMessage, ChatID, message));
+
+                await api.Messages.SendAsync(new MessagesSendParams
+                {
+                    UserId = ChatID,
+                    Attachments = MessageAttachment,
+                    RandomId = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                });
+
+                File.Delete(path);
+            }
+            else
+            {
+                await api.Messages.SendAsync(new MessagesSendParams
+                {
+                    UserId = ChatID,
+                    Message = message,
+                    RandomId = DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+                });
+            }
+
+            if (message != "" && speechButton.IsChecked == true)
+                Chat.Text += "\n\n" + message + "(голос) ---- Вы";
+            else
+                Chat.Text += "\n\n" + message + " ---- Вы";
+
+            Chat.ScrollToEnd();
+
+            HideLoadingScreen();
+        }
+
+        string FormatPath(string path)
+        {
+            char[] badSymbols = { '*', '"', '?', '>', '<', '|', '*', '+' };
+
+            foreach (char symbol in badSymbols)
+            {
+                path = path.Replace(symbol, ' ');
+            }
+
+            if (path.Length > 245)
+                path = path.Remove(245, path.Length - 245);
+
+            return path;
         }
 
         void HideCapchaWindow()
@@ -211,6 +463,50 @@ namespace vk_tts
             });
 
             HideLoadingScreen();
+        }
+
+        /// <summary>
+        /// Загружает документ на сервер ВК.
+        /// </summary>
+        /// <param name="vkApi">Вк апи.</param>
+        /// <param name="data">Аттачмент, байты которого будут отправлены на сервер</param>
+        /// <param name="docMessageType">Тип документа - документ или аудиосообщение.</param>
+        /// <param name="peerId">Идентификатор назначения</param>
+        /// <param name="filename">Итоговое название документа</param>
+        /// <returns>Аттачмент для отправки вместе с сообщением.</returns>
+        public async Task<MediaAttachment> LoadDocumentToChatAsync(VkApi vkApi, byte[] data,
+            DocMessageType docMessageType, long peerId, string filename)
+        {
+            var uploadServer = vkApi.Docs.GetMessagesUploadServer(peerId, docMessageType);
+
+            var r = await UploadFile(uploadServer.UploadUrl, data);
+            var documents = vkApi.Docs.Save(r, filename ?? Guid.NewGuid().ToString());
+
+            if (documents.Count != 1)
+                throw new ArgumentException($"Error while loading document attachment to {uploadServer.UploadUrl}");
+
+            return documents[0];
+        }
+
+        /// <summary>
+        /// Загружает массив байт на указанный url
+        /// </summary>
+        /// <param name="url">Адрес для загрузки</param>
+        /// <param name="data">Массив данных для загрузки</param>
+        /// <returns>Строка, которую вернул сервер.</returns>
+        public async Task<string> UploadFile(string url, byte[] data)
+        {
+            using (var client = new HttpClient())
+            {
+                var requestContent = new MultipartFormDataContent();
+                var documentContent = new ByteArrayContent(data);
+                documentContent.Headers.ContentType = new MediaTypeHeaderValue("multipart/form-data");
+                requestContent.Add(documentContent, "file", "audio.webm");
+
+                var response = await client.PostAsync(url, requestContent);
+
+                return Encoding.ASCII.GetString(await response.Content.ReadAsByteArrayAsync());
+            }
         }
     }
 
@@ -281,6 +577,12 @@ namespace vk_tts
             array[index2] = temp;
         }
     }
+
+
 }
 
-
+public class LongPollResponse
+{
+    public ulong ts;
+    public string[][] updates;
+}
